6217d190ed46ece022d961891114e832e8ccf383
henrysalim <henrysalim22@gmail.com>
2025-11-09 22:34:16 +0700

    added description text to each section

--- src/components/CorePrinciples.vue
+++ src/components/CorePrinciples.vue
@@ -1,3 +1,60 @@
-<script setup></script>
+<script setup>
+import SectionHeading from "./SectionHeading.vue";
 
-<template></template>
+let corePrinciples = [
+  {
+    title: "Integrity",
+    description:
+      "We uphold honesty and responsibility in every step of our work.",
+  },
+  {
+    title: "Innovation",
+    description:
+      "We are constantly seeking new ways to create more effective and impactful solutions.",
+  },
+  {
+    title: "Collaboration",
+    description:
+      "We believe the best results stem from solid teamwork and communication.",
+  },
+  {
+    title: "Excellence",
+    description:
+      "We are committed to delivering the best results, going beyond merely meeting expectations.",
+  },
+  {
+    title: "Sustainability",
+    description:
+      "We focus on long-term growth that benefits both our clients and the community.",
+  },
+];
+</script>
+
+<template>
+  <main class="max-w-6xl mx-10 lg:mx-auto h-screen py-10">
+    <section id="core-principles">
+      <SectionHeading title="Our Core Values" />
+
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <div
+          class="p-6 rounded-xl shadow-xl"
+          v-for="(principleData, idx) in corePrinciples"
+          v-show="idx != 4"
+        >
+          <h3 class="mb-4 text-3xl font-semibold text-center">
+            {{ principleData.title }}
+          </h3>
+          <p>{{ principleData.description }}</p>
+        </div>
+      </div>
+      <div class="w-full mt-6">
+        <div class="p-6 rounded-xl shadow-xl">
+          <h3 class="mb-4 text-3xl font-semibold text-center">
+            {{ corePrinciples[4].title }}
+          </h3>
+          <p class="text-center">{{ corePrinciples[4].description }}</p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
---
+++ src/components/SectionHeading.vue
@@ -0,0 +1,7 @@
+<script setup>
+defineProps(["title"]);
+</script>
+
+<template>
+  <h2 class="text-3xl lg:text-4xl xl:text-5xl font-bold mb-4">{{ title }}</h2>
+</template>
